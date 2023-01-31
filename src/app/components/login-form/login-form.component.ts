8f4c575c2261fd52dcfbd7fec5e847f629356621
Arif Hossain <hossaininfo@ymail.com>
2023-01-31 14:30:38 +0100

    added UserService to the constructor and create and save user

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {LoginService} from "../../services/login.service";
 import {NgForm} from "@angular/forms";
 import {User} from "../../models/user.model";
-import {Router} from "@angular/router";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-login-form',
@@ -11,9 +11,13 @@ import {Router} from "@angular/router";
 })
 export class LoginFormComponent {
 
+  // Create EventEmitter which emitt event to the parent component that is hosting to login form
+  @Output() login: EventEmitter<void> = new EventEmitter();
+
+  // DI
   constructor(
-    private readonly router: Router,
-    private readonly loginService: LoginService
+    private readonly loginService: LoginService,
+    private readonly userService: UserService,
   ) {}
 
   public  loginSubmit(loginForm: NgForm): void{
@@ -23,10 +27,11 @@ export class LoginFormComponent {
     this.loginService.login(username)
       .subscribe({
         next: (user: User)=>{
-          this.router.navigateByUrl("/pokemon")
+          this.userService.user = user;
+          this.login.emit();
         },
         error: ()=>{
-
+          // Handle error locally
         }
       })
   }
