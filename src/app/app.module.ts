92224b64c973b192971bb6e2fbd8f6ef3c1c38a8
Arif Hossain <hossaininfo@ymail.com>
2023-01-31 08:42:40 +0100

    created login services and added HttpClientModule to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {AppRoutingModule} from "./app-routing.module";
+import {AppRoutingModule} from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginPage } from './pages/login/login.page';
 import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
 import { ProfilePage } from './pages/profile/profile.page';
 import { LoginFormComponent } from './components/login-form/login-form.component';
-import {HttpClientModule} from "@angular/common/http";
 
 // Decorator
 @NgModule({
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,57 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {map, Observable, of, switchMap} from "rxjs";
+import {User} from "../models/user.model";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+
+const {apiUsers, apiKey} = environment
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) {
+  }
+
+  //Login
+  public login(username: string): Observable<User> {
+    return this.checkUsername(username)
+      .pipe(
+        switchMap((user: User | undefined) => {
+          if (user === undefined) {
+            return this.createUser(username);
+          }
+          return of(user);
+        })
+      )
+  }
+
+  // Check if user exist
+  private checkUsername(username: string): Observable<User | undefined> {
+    return this.http.get<User[]>(`${apiUsers}?username=${username}`)
+      .pipe(
+        map((response: User[]) => response.pop())
+      )
+  }
+
+  //Create user
+
+  private createUser(username: string): Observable<User> {
+    const user = {
+      username,
+      pokemon: []
+    };
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "x-api-key": apiKey
+    })
+
+    return this.http.post<User>(apiUsers, user, {
+      headers
+    })
+
+  }
+
 }
