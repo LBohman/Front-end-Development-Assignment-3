1cf2e6d2b240c08a3038e5b72c6f04335611075a
Arif Hossain <hossaininfo@ymail.com>
2023-01-31 15:14:52 +0100

    activated auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,27 +3,31 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginPage} from "./pages/login/login.page";
 import {PokemonCataloguePage} from "./pages/pokemon-catalogue/pokemon-catalogue.page";
 import {ProfilePage} from "./pages/profile/profile.page";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes=[
+const routes: Routes = [
   {
-    path:"",
-    pathMatch:"full",
-    redirectTo:"/login"
+    path: "",
+    pathMatch: "full",
+    redirectTo: "/login"
   },
   {
-    path:"login",
+    path: "login",
     component: LoginPage
   },
   {
-    path:"pokemon",
-    component: PokemonCataloguePage
+    path: "pokemon",
+    component: PokemonCataloguePage,
+    canActivate: [AuthGuard]
   },
   {
-  path:"profile",
-    component:ProfilePage
+    path: "profile",
+    component: ProfilePage,
+    canActivate: [AuthGuard]
   },
 
 ]
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
