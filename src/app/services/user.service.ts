3eff8cb7dfe4165ea12f15359962d93d550028e5
Arif Hossain <hossaininfo@ymail.com>
2023-01-31 14:28:14 +0100

    created getter and setter to UserService and when create user save them to sessionStorage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {User} from "../models/user.model";
+import {StorageUtil} from "../utils/storage.util";
+import {StorageKeys} from "../enums/storage-keys.enums";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private _user?: User;
+  public get user(): User | undefined {
+    return this._user;
+  }
 
-  constructor() { }
+  public set user(user: User | undefined) {
+    StorageUtil.storageSave<User>(StorageKeys.User, user!); // user! means it is never undefined
+    this._user = user;
+  }
+
+
+  constructor() {
+    this._user = StorageUtil.storageRead<User>(StorageKeys.User);
+  }
 }
