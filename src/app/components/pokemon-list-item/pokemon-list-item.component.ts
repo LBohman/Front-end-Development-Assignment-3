5caa3268138da5985dc63f10e42f42b5a92e096f
Arif Hossain <hossaininfo@ymail.com>
2023-02-03 18:35:27 +0100

    added collected button

--- src/app/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,14 +1,18 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import {Component, Input} from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
 import { User } from 'src/app/models/user.model';
 import {Pokemon} from "../../models/pokemon.model";
 import {PokemonService} from "../../services/pokemon.service";
+
+
 
 @Component({
   selector: 'app-pokemon-list-item',
   templateUrl: './pokemon-list-item.component.html',
   styleUrls: ['./pokemon-list-item.component.css']
 })
+
+
 export class PokemonListItemComponent {
   @Input() pokemon?: Pokemon;
 
@@ -16,7 +20,9 @@ export class PokemonListItemComponent {
     return this.pokemonService.loading;
   }
 
-  constructor(private readonly pokemonService: PokemonService) {}
+  constructor(
+    private readonly pokemonService: PokemonService
+  ) {}
 
   getPokemonImage() {
   const pokemonId = this.pokemonService.geturl(this.pokemon?.url)
@@ -24,12 +30,14 @@ export class PokemonListItemComponent {
   return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`
   }
 
+
   addPokemonToCollection(): void {
     // this.pokemon.isFavorite = !this.pokemon.isFavorite
     this.pokemonService.addToCollection(this.pokemon!.name)
       .subscribe({
         next: (response: User) => {
           console.log("NEXT", response);
+          this.pokemon!.isInCollection = true;
         },
         error: (error: HttpErrorResponse) => {
           console.log(error.message);
