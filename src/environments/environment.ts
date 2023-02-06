aaf112f78a1d4666e4d29b9e862f3c0e09d680db
Lovisa <lovisa.bohman@se.experis.com>
2023-02-06 16:28:02 +0100

    Adding the environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiKey: "eN0QMoEhe0mlgA9ECOO0Aw==",
+  apiUsers: "https://noroff-assignment-api-production-90f7.up.railway.app/trainers",
+  apiPokemon: "https://pokeapi.co/api/v2/pokemon?limit=151",
+};
